7151489706401673fdf561747276a882658e5def
Yuki Koyamatsu <wxiao919@gmail.com>
2018-03-18 07:49:01 +0000

    指数オーダーのfub関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+function fub(n){
+    if(n == 0){
+        return 0 ;
+    }else if (n == 1){
+        return 1 ;
+    }
+    return fub(n-2)+fub(n-1);
+}
+const length = 40 ;
+for(let i = 0 ; i <= length; i++){
+    console.log(fub(i));
+}
